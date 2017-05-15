02997db3e9e3f9140d6960f3f7ad09f2555277d5
Elise St Hilaire and Jiwon Kang <elise+jiwon@>
2017-05-15 16:10:15 -0700

    Create products service

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+// import { PRODUCTS } from './mock-products';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class ProductService {
+  products: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.products = database.list('products');
+  }
+
+  getProducts() {
+    return this.products;
+  }
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-  products: Product[] = [
-    new Product("Super Duper Pantelones", "Male", "Pants", 70, "Medium", "Blue", 100, "These are great for wearing around an office", "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjo5Yii8fLTAhUN5WMKHQsZC1MQjRwIBw&url=http%3A%2F%2Fwww.patagonia.com%2Fshop%2Fmens-pants&psig=AFQjCNF2UNKWZOYePa6N4O0TzOs_wcMnPg&ust=1494971571321280" ,1),
-    new Product("Fancy Blazer", "Female", "Jacket", 50, "Small", "Black", 60, "These are great for wearing around an office", "https://s-media-cache-ak0.pinimg.com/originals/71/11/b0/7111b0a3dbd560b8fcf87e47769ce1a5.jpg" ,2)
-  ];
+export class ProductsComponent implements OnInit {
+  products: FirebaseListObservable<any[]>;
 
+  ngOnInit() {
+    
+  }
 }
