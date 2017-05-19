190322867a86b4fb5c13fb031089eb83cde0b855
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-19 09:25:17 -0700

    changed style pages to scss in .ts files

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,7 +6,7 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css'],
+  styleUrls: ['./products.component.scss'],
   providers: [ ProductService ]
 })
 
