29d06111e5e630b1daa486ed75d2461366f49f25
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-19 17:04:11 -0700

    Create input decorator to pass selected item to add to bag

--- src/app/added-to-bag/added-to-bag.component.ts
+++ src/app/added-to-bag/added-to-bag.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from './../product.model';
 
 @Component({
   selector: 'app-added-to-bag',
   templateUrl: './added-to-bag.component.html',
   styleUrls: ['./added-to-bag.component.scss']
 })
+
+
 export class AddedToBagComponent implements OnInit {
+  @Input() productToDisplay = Product;
   showAlert: boolean = false;
   addToBag() {
     this.showAlert=true;
