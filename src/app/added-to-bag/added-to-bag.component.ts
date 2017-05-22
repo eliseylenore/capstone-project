5eed6f7478e490c299a39fbdbbaf074585c86b9f
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-22 10:01:25 -0700

    Successfully created one product to add to bag

--- src/app/added-to-bag/added-to-bag.component.ts
+++ src/app/added-to-bag/added-to-bag.component.ts
@@ -14,20 +14,20 @@ export class AddedToBagComponent {
   constructor(private productService: ProductService) { }
 
   @Input() productToDisplay = Product;
-
   showAlert: boolean = false;
 
-  newProduct(thisProduct: Product) {
-    var name = thisProduct.name;
-    var gender = thisProduct.gender;
-    var clothing = thisProduct.clothing;
-    var price = thisProduct.price;
-    var size = thisProduct.size;
-    var color = thisProduct.color;
-    var description = thisProduct.description;
-    var imageUrl = thisProduct.imageUrl;
-    var newProductToAdd: Product = new Product(name, gender, clothing, price, size, color, description, imageUrl);
-    this.productService.addProduct(newProductToAdd);
+  newProduct(
+    name: string,
+    gender: string,
+    clothing: string,
+    price: number,
+    size: string,
+    color: string,
+    description: string,
+    imageUrl: string) {
+      console.log("chosen product: " + name);
+      var newProduct = new Product(name, gender, clothing, price, size, color, description, imageUrl);    
+      this.productService.addProduct(newProduct);
   }
 
   addToBag() {
