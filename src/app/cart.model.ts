7d28de35618049eb92429e4b6c0be439ded640f8
Elise St Hilaire <eliseylenore@gmail.com>
2017-05-22 21:42:17 -0700

    refactor calculated property to arrow syntax

--- src/app/cart.model.ts
+++ src/app/cart.model.ts
@@ -5,14 +5,18 @@ export class Cart {
 	constructor (
     public products: Product[],
   	) {
-			this.totalPrice = this.determineTotalPrice(this.products);
+			this.totalPrice = this.products.reduce((price: number, product) => {
+				let productPrice = Number(product.price);
+				return price + productPrice;
+			}, 0)
+			// this.totalPrice = this.determineTotalPrice(this.products);
 		}
 
-		determineTotalPrice(products) {
-			var totalPrice = 0;
-			products.forEach((product) =>
-				totalPrice += product.price * product.quantity
-			);
-			return totalPrice;
-		}
+		// determineTotalPrice(products) {
+		// 	var totalPrice = 0;
+		// 	products.forEach((product) =>
+		// 		totalPrice += product.price * product.quantity
+		// 	);
+		// 	return totalPrice;
+		// }
 }
