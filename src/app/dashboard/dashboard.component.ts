427f52f518b745605b70a5f13ff9163fb6666c25
Elise St Hilaire <eliseylenore@gmail.com>
2017-07-20 12:19:18 -0700

    Add styles to login

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,10 +8,18 @@ import { AuthenticationService } from '../authentication.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
 
   constructor(public authService: AuthenticationService) {
     this.authService.user.subscribe(user => {
-      console.log(user);
+      if(user === null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+      }
     });
   }
 
