74d7493ff96dc06813e0eea8affb4b05eee2fd25
Elise St Hilaire <eliseylenore@gmail.com>
2017-07-20 15:38:47 -0700

    Create dropdown at small viewport width

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationService } from '../authentication.service';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,8 +13,11 @@ export class DashboardComponent {
   user;
   private isLoggedIn: Boolean;
   private userName: String;
+  innerHeight: any;
+  innerWidth: any;
+  isWideEnough: Boolean;
 
-  constructor(public authService: AuthenticationService) {
+  constructor(public authService: AuthenticationService, private router: Router, private http: Http) {
     this.authService.user.subscribe(user => {
       if(user === null) {
         this.isLoggedIn = false;
@@ -20,6 +25,9 @@ export class DashboardComponent {
         this.isLoggedIn = true;
         this.userName = user.displayName;
       }
+      this.innerHeight = (window.screen.height) + "px";
+      this.innerWidth = (window.screen.width) + "px";
+      this.isWideEnough = innerWidth > 691;
     });
   }
 
