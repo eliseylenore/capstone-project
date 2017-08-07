d2d54eafd05d82cfe92fe0dc349dcb58d1075493
Elise St Hilaire <eliseylenore@gmail.com>
2017-08-07 16:10:51 -0700

    search bar successfully redirects to search page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Subject } from 'rxjs/Subject';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -10,21 +11,28 @@ import { Subject } from 'rxjs/Subject';
 })
 export class SearchComponent implements OnInit {
   products;
+  search;
   startAt = new Subject();
   endAt = new Subject();
 
   lastKeypress: number = 0; //event timestamp in milliseconds
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.productService.getProducts()
-      .subscribe(products => this.products = products)
+    this.route.params.forEach((urlParameters) => {
+      this.search = urlParameters['search'];
+    });
+    this.products = this.productService.getSearchedProducts(this.search);
   }
 
-  search(search) {
+  searchThis(search) {
     this.products = this.productService.getSearchedProducts(search);
     console.log("products: " + JSON.stringify(this.products));
   }
+
+  goToDetailPage(clickedProduct) {
+    this.router.navigate(['products', clickedProduct.$key]);
+  };
 
 }
